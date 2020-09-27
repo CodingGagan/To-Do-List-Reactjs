84033c1160b96cc9222f72dba1a7760755a270da
Learning <learningphp11@gmail.com>
2020-09-28 00:58:15 +0530

    Updates

---
+++ precache-manifest.daea5fda4830adbab52102f3465441c5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62085b0588594df711fcfdbed0c194eb",
+    "url": "/To-Do-List-Reactjs/index.html"
+  },
+  {
+    "revision": "b09d7ac67c2a3b31a581",
+    "url": "/To-Do-List-Reactjs/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "941f6fc166dd64c45eaa",
+    "url": "/To-Do-List-Reactjs/static/css/main.f2ce1808.chunk.css"
+  },
+  {
+    "revision": "b09d7ac67c2a3b31a581",
+    "url": "/To-Do-List-Reactjs/static/js/2.8bd11abd.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/To-Do-List-Reactjs/static/js/2.8bd11abd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "941f6fc166dd64c45eaa",
+    "url": "/To-Do-List-Reactjs/static/js/main.e3b19d47.chunk.js"
+  },
+  {
+    "revision": "16a0d640f19ca393345e",
+    "url": "/To-Do-List-Reactjs/static/js/runtime-main.446e4361.js"
+  }
+]);
